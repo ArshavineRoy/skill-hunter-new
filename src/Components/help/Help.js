9316c9dc50c88054e059183a465ae9ff685b51d5
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-09 23:44:27 +0300

    refactor Help component - by Mercy

--- src/Components/help/Help.js
+++ src/Components/help/Help.js
@@ -1,27 +1,31 @@
 import React from "react";
-import {NavLink, Outlet} from 'react-router-dom'
 
-
-function Help() {
-
-  return <div className="help-layout">
-    <div className="help-layout-content">
-
-     <h2>Website Help</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem dicta
-        illum, excepturi saepe
-      </p>
-    </div>
-    <nav>
-      <NavLink to="faq">View FAQ</NavLink>
-      <NavLink to="contact">Contac us</NavLink> 
-    </nav>
-
-    <main>
-      <Outlet/>
-    </main>
-  </div>;
+function ContactUs() {
+  return (
+   <div>
+     <h1 className="text-blue-700 mb-2" >Customer Care Support Team</h1>
+     <span>phone number : +254786464433</span>
+     <div> 
+      <span>Email : customercare@skillhunter.co.ke</span>
+      </div>
+       <div>
+         <h1 className="text-blue-700 mt-3 py-2" >Online Support Team</h1>
+         <span>Twitter : @SkillHunter_care</span>
+        <div> 
+          <span> Facebook : @SkillHunter</span>
+        </div>
+        <div> 
+          <h1 className="text-blue-700 mt-3 py-2">Careers and HR</h1>
+          <span>General Queries : hr@skillhunter.co.ke </span>
+        <div>
+          <h1 className="text-blue-700 mt-3 py-2">CEO's Office</h1>
+          <span>Email : ceo@skillhunter.co.ke</span>
+        </div>
+        </div>
+      </div>
+   </div>
+  
+  );
 }
 
-export default Help;
+export default ContactUs;
