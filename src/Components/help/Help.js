86fb7d9fb26e87c84e4b893863e8a754b8aa70f1
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-10 00:23:54 +0300

    refactor FAQ, Contact US, Help components     - add faqs to json file     - remove NavLinks to FAQ and Contact     - fetch faq data from server

--- src/Components/help/Help.js
+++ src/Components/help/Help.js
@@ -1,31 +1,23 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import Faq from "./Faq";
+import ContactUs from "./ContactUs";
 
-function ContactUs() {
+function Help() {
   return (
-   <div>
-     <h1 className="text-blue-700 mb-2" >Customer Care Support Team</h1>
-     <span>phone number : +254786464433</span>
-     <div> 
-      <span>Email : customercare@skillhunter.co.ke</span>
+    <div className="help-layout">
+      <div className="help-layout-content">
+        {/* <h3 className="font-bold" >Website Help</h3> */}
+        <Faq />
+
+        <h3 className="text-xl text-center font-bold mt-16 mb-4">
+          For more information, reach out to us via the contact info below:
+        </h3>
+
+        <ContactUs />
       </div>
-       <div>
-         <h1 className="text-blue-700 mt-3 py-2" >Online Support Team</h1>
-         <span>Twitter : @SkillHunter_care</span>
-        <div> 
-          <span> Facebook : @SkillHunter</span>
-        </div>
-        <div> 
-          <h1 className="text-blue-700 mt-3 py-2">Careers and HR</h1>
-          <span>General Queries : hr@skillhunter.co.ke </span>
-        <div>
-          <h1 className="text-blue-700 mt-3 py-2">CEO's Office</h1>
-          <span>Email : ceo@skillhunter.co.ke</span>
-        </div>
-        </div>
-      </div>
-   </div>
-  
+    </div>
   );
 }
 
-export default ContactUs;
+export default Help;
--- src/data/DataContextProvider.js
+++ src/data/DataContextProvider.js
@@ -4,6 +4,7 @@ import { useContext, useState, useEffect } from "react";
 const dataContext = createContext();
 function DataContextProvider({ children }) {
   const [careerData, setCareerData] = useState([]);
+  const [faqData, setFaqData] = useState([])
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -11,14 +12,21 @@ function DataContextProvider({ children }) {
       .then((res) => res.json())
       .then((data) => setCareerData(data))
       .finally(setIsLoading(false));
+
+    fetch(`http://localhost:3000/faq`)
+      .then((res) => res.json())
+      .then((data) => setFaqData(data))
+      .finally(setIsLoading(false));
   }, []);
 
-  const values = { careerData, setCareerData, setIsLoading };
+  const values = { careerData, setCareerData, setIsLoading, faqData, setFaqData };
   return <dataContext.Provider value={values}>{children}</dataContext.Provider>;
 }
 
 export default DataContextProvider;
 export { dataContext };
+
+//https://skill-hunter-server.onrender.com/careers
 
 //https://skill-hunter-server.onrender.com/careers
 //http://localhost:3000/careers
